7d239b1a6cb066592a4a590ee0578b2a8475854b
Fernando Araoz <fernando@araozu.dev>
2026-01-13 18:46:14 -0500

    Create index.ts

---
+++ src/lib/infra/db/index.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
+import { env } from '$env/dynamic/private';
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
+
+if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+const sqlite = new Database(env.DATABASE_URL);
+export const db = drizzle(sqlite);
+
+migrate(db, { migrationsFolder: './drizzle' });
