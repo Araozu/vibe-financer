d441014eb08ff13ccaaf0e912aba8df3bf371863
Fernando Araoz <fernando@araozu.dev>
2026-01-12 19:27:18 -0500

    feat: bun sqlite

--- src/lib/infra/db/index.ts
+++ src/lib/infra/db/index.ts
@@ -1,7 +1,17 @@
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-import Database from 'better-sqlite3';
+// import { drizzle } from 'drizzle-orm/better-sqlite3';
+// import Database from 'better-sqlite3';
+// import { env } from '$env/dynamic/private';
+// import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+
+// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+// const sqlite = new Database(env.DATABASE_URL);
+// export const db = drizzle(sqlite);
+
+import { drizzle } from "drizzle-orm/bun-sqlite";
+import { Database } from "bun:sqlite";
 import { env } from '$env/dynamic/private';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
