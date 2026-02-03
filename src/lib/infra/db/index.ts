f843d313836d030b98ee881922f766523feb1c08
Fernando Araoz <fernando@araozu.dev>
2026-02-03 07:14:07 -0500

    feat: deployment setup

--- src/lib/infra/db/index.ts
+++ src/lib/infra/db/index.ts
@@ -5,6 +5,14 @@ import { migrate } from 'drizzle-orm/bun-sqlite/migrator';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
+// If the database is not in-memory, check if the file exists, and create it if not using Bun APIs.
+if (env.DATABASE_URL !== ':memory:') {
+    const exists = await Bun.file(env.DATABASE_URL).exists();
+    if (!exists) {
+        await Bun.write(env.DATABASE_URL, "");
+    }
+}
+
 const sqlite = new Database(env.DATABASE_URL);
 export const db = drizzle(sqlite);
 
