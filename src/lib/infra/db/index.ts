c1693d8d930a58c63064939fe815b99721b980af
Fernando Araoz <fernando@araoz.dev>
2026-02-23 07:25:45 -0500

    feat: add database migration support using drizzle-orm

--- src/lib/infra/db/index.ts
+++ src/lib/infra/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import pg from 'pg';
 import { env } from '$env/dynamic/private';
 
@@ -11,3 +12,4 @@ const pool = new pg.Pool({
 });
 
 export const db = drizzle(pool);
+await migrate(db, { migrationsFolder: './drizzle' });
