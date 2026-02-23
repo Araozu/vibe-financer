ae76533ab26a0f6ddcb0eb82ccd2ae7e91580c7f
Fernando Araoz <fernando@araoz.dev>
2026-02-23 07:21:43 -0500

    feat: log DATABASE_URL for debugging purposes

--- src/lib/infra/db/index.ts
+++ src/lib/infra/db/index.ts
@@ -4,6 +4,8 @@ import { env } from '$env/dynamic/private';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
+console.log('DATABASE_URL: ', env.DATABASE_URL);
+
 const pool = new pg.Pool({
 	connectionString: env.DATABASE_URL
 });
